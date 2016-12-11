a61eaf5ce220c0ce1d73a2b6a07dcff40c4a3583
anachronism <mhli@wpi.edu>
2016-12-11 18:30:01 -0500

    Kalman filter model is a lot closer than it was before

--- MATLAB_Scripts/Kalman_sim_gps.m
+++ MATLAB_Scripts/Kalman_sim_gps.m
@@ -7,7 +7,8 @@ dt = 0.01;
 A = [1 0 dt 0; 0 1 0 dt; 0 0 1 0; 0 0 0 1];
 %Initialize x to 0;
 x = [0;0;0;0;];
-
+q = 1;
+sd_r = 1;
 %Initialize input
 z = zeros(4, numTrials);
 for i = 1:numTrials
@@ -22,11 +23,14 @@ var_vy = 1;
 p= [var_dx 0 0 0; 0 var_dy 0 0; 0 0 var_vx 0; 0 0 0 var_vy];
 %initialize H
     H = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
-    %Q is noise
+    %Q is noise covariance
     %Q = [0.01 0.01 0.011 0.1; 0.01 0.01 0.1 0.01; 0.3 0.02 0.01 0.23; 0.054 0.13 0.1 0.01];
-    Q = [0.25 0 0.5 0; 0 0.25 0 0.5; 0.5 0 1 0; 0 0.5 0 1];
+    Q = q*[1/4*dt^4 0 0.5*dt^3 0; 0 1/4*dt^4 0 0.5*dt^3; 0.5*dt^3 0 dt^2 0; 0 0.5*dt^3 0 dt^2];
+    %Q = zeros(4,4);
+    %Q(4,4) = 1;
+    
     %R = zeros(4,4);
-    R = eye(4);
+    R = sd_r * [1/4*dt^4 0 0.5*dt^3 0; 0 1/4*dt^4 0 0.5*dt^3; 0.5*dt^3 0 dt^2 0; 0 0.5*dt^3 0 dt^2];
     
 for i = 1:numTrials
     %predict Step
